1b927c44e7f5640158d30194801c735cbeaa8116
Andrea Tentner <andreat8@gmail.com>
2026-04-01 12:48:17 -0500

    update model registrations to allow better visibility into the restaurant permit model

--- map/admin.py
+++ map/admin.py
@@ -3,10 +3,11 @@ from .models import CommunityArea,RestaurantPermit
 
 @admin.register(CommunityArea)
 class CommunityAreaAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name','area_id')
 
 @admin.register(RestaurantPermit)
 class RestaurantPermitAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('permit_id', 'permit_type', 'community_area_id','issue_date','location')
+    list_filter = ('permit_type','community_area_id','issue_date')
 
 
