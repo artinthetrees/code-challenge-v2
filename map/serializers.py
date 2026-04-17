c0b0013f69d1c8a5bb983a1bc38f511757df44fd
Andrea Tentner <andreat8@gmail.com>
2026-04-17 17:21:11 -0500

    add calculated percent_permits field to CA serializer

--- map/serializers.py
+++ map/serializers.py
@@ -26,10 +26,10 @@ from map.models import CommunityArea, RestaurantPermit
 class CommunityAreaSerializer(serializers.ModelSerializer):
     class Meta:
         model = CommunityArea
-        fields = ["name", "area_id","num_permits"]
+        fields = ["name", "area_id","num_permits","percent_permits"]
 
     num_permits = serializers.SerializerMethodField(method_name='get_num_permits')
-    #percent_permits = serializers.SerializerMethodField(method_name='get_percent_permits')
+    percent_permits = serializers.SerializerMethodField(method_name='get_percent_permits')
 
 
     def get_num_permits(self, obj):
@@ -39,4 +39,13 @@ class CommunityAreaSerializer(serializers.ModelSerializer):
         restaurant_permits = RestaurantPermit.objects.filter(community_area_id=current_area_id,issue_year=current_year)
 
         return restaurant_permits.count()
+
+    def get_percent_permits(self,obj):
+
+        # Fetch the restaurant permits associated with the community area using filter
+        current_area_id = obj.area_id
+        current_year = self.context["year"]
+        restaurant_permits = RestaurantPermit.objects.filter(community_area_id=current_area_id,issue_year=current_year)
+        restaurant_permits_all = RestaurantPermit.objects.filter(issue_year=current_year)
         
+        return 100*(restaurant_permits.count()/restaurant_permits_all.count())   
