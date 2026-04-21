0ea11021b7408b6aa436db536d6065c18ddd0e17
Andrea Tentner <andreat8@gmail.com>
2026-04-21 12:57:48 -0500

    get quartile values for percent permits; get ca fill color based on which percent permits quartile the ca is in during the selected year and apply that fill color on the map

--- map/static/js/RestaurantPermitMap.js
+++ map/static/js/RestaurantPermitMap.js
@@ -77,11 +77,55 @@ export default function RestaurantPermitMap() {
   const maxPermits = Math.max(...currentYearData.map(item => item.num_permits));
   console.log('Max Permits:', maxPermits); 
 
+  // Get quartiles of percent permits 
+  const getQuartiles = (arr, key) => {
+    // 1. Extract key values from each dictionary in array of dictionaries and sort values numerically
+    const values = arr.map(item => item[key]).sort((a, b) => a - b);
+    
+    const quantile = (sortedArr, q) => {
+      const pos = (sortedArr.length - 1) * q;
+      const base = Math.floor(pos);
+      const rest = pos - base;
+      if (sortedArr[base + 1] !== undefined) {
+        return sortedArr[base] + rest * (sortedArr[base + 1] - sortedArr[base]);
+      } else {
+        return sortedArr[base];
+      }
+    };
+
+    return {
+      "q1": quantile(values, 0.25),
+      "q2": quantile(values, 0.50), // Median
+      "q3": quantile(values, 0.75)
+    };
+  };
+
+  const permitPercentQuartiles = getQuartiles(currentYearData,'percent_permits')
+  console.log('Percent Permits Quartiles:', permitPercentQuartiles);
+
   function getColor(percentageOfPermits) {
     /**
      * TODO: Use this function in setAreaInteraction to set a community 
      * area's color using the communityAreaColors constant above
      */
+    console.log('check percentageOfPermits:', percentageOfPermits);
+    console.log('check permitPercentQuartiles:', permitPercentQuartiles.q1);
+    console.log('check permitPercentQuartiles:', permitPercentQuartiles.q2);
+    console.log('check permitPercentQuartiles:', permitPercentQuartiles.q3);
+    if (percentageOfPermits <= permitPercentQuartiles.q1) {
+      console.log('check:', '1');
+      return communityAreaColors[0];
+    } else if (percentageOfPermits <= permitPercentQuartiles.q2) {
+      console.log('check:', '2');
+      return communityAreaColors[1];
+    } else if (percentageOfPermits <= permitPercentQuartiles.q3) {
+      console.log('check:', '3');
+      return communityAreaColors[2];
+    } else {
+      console.log('check:', '4');
+      return communityAreaColors[3];
+    }
+
   }
 
   function setAreaInteraction(feature, layer) {
@@ -93,22 +137,30 @@ export default function RestaurantPermitMap() {
      * permit count for the year
      */
     const ca_name = feature?.properties?.community || "Unknown";
-    // const ca_model_data = currentYearData.find(ca_entry => ca_entry.name === ca_name);
-    const ca_model_data = currentYearData.at(0);
-    const result = ca_model_data ?? "Default String"; 
-    const result2 = currentYearData?.length ?? 0;
+    console.log('ca name:', ca_name); 
+    const ca_model_data = currentYearData.find(ca_entry => ca_entry.name === ca_name);
+    console.log('ca model data:', ca_model_data);
+    const ca_model_data_str = JSON.stringify(ca_model_data, null, 2)
+    
+    console.log('Percent Permits Quartiles:', permitPercentQuartiles);
+    let ca_color = getColor(ca_model_data.percent_permits)
+    console.log('ca color:', ca_color);
+    // const ca_model_data = currentYearData.at(0);
+    // const result = ca_model_data ?? "Default String"; 
+    // const result2 = currentYearData?.length ?? 0;
     // const ca_raw_permit_count = ca_model_data.num_permits;
     // const ca_raw_permit_count_str = ca_raw_permit_count.toString();
     // const ca_popup = `${ca_name}, ${ca_raw_permit_count_str}`;
     layer.setStyle({
-      fillColor: '#ff0000', // Red fill color for shading
+      // fillColor: '#ff0000', // Red fill color for shading
+      fillColor: ca_color, // Red fill color for shading
       fillOpacity: 0.5,    // Semi-transparent
       color: '#000000',    // Border color
       weight: 2            // Border weight
     })
     layer.on("mouseover", () => {
       // layer.bindPopup(result2.toString())
-      layer.bindPopup(result2.toString())
+      layer.bindPopup(ca_model_data_str)
       layer.openPopup()
     })
   }
@@ -121,8 +173,6 @@ export default function RestaurantPermitMap() {
       </p>
       <p className="fs-4">
         Maximum number of restaurant permits in a single area: { maxPermits }
-        {/* {  testyearlyDataEndpoint  } */}
-        {/* { currentYearData.length } */}
       </p>
       <MapContainer
         id="restaurant-map"
@@ -133,17 +183,18 @@ export default function RestaurantPermitMap() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}@2x.png"
         />
-        <GeoJSON 
+        {/* <GeoJSON 
           data={RAW_COMMUNITY_AREAS} 
           onEachFeature={setAreaInteraction}
-        />
-        {/* {currentYearData.length > 0 ? (
+          key={maxPermits}
+        /> */}
+        {currentYearData.length > 0 ? (
           <GeoJSON
             data={RAW_COMMUNITY_AREAS}
             onEachFeature={setAreaInteraction}
-            key={maxNumPermits}
+            key={maxPermits}
           />
-        ) : null} */}
+        ) : null}
       </MapContainer>
     </>
   )
